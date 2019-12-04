710b37aa9a6f23b15e770b20a8ffd2cfb4f8ff34
Edward Hsu <ad21444@yahoo.com.tw>
2019-12-04 14:11:44 +0800

    add guess times

--- gn.py
+++ gn.py
@@ -1,12 +1,16 @@
 import random
 n = random.randint(1,100)
 i = -1
-while i<=0:
+a = 0
+while True:
 	num = int(input('請輸入一個1-100之間的整數'))
+	a = a +1
 	if n == num :
-		print('正確')
-		i = 1
+		print('答案正確')
+		print('共猜了', a ,'次')
+		break
 	elif n >num :
-		print('比答案大')
+		print('猜的數字比答案小')
 	else:
-		print('比答案小')
+		print('猜的數字比答案大')
+	print('已經猜了', a ,'次')
